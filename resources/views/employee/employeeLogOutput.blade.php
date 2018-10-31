cf876d012ac7dc08464dbc88cf404877e0c8456d
Holly Stone <hstone102@gmail.com>
2018-10-31 13:51:08 -0400

    out forms done

--- resources/views/employee/employeeLogOutput.blade.php
+++ resources/views/employee/employeeLogOutput.blade.php
@@ -125,6 +125,240 @@
     </span>
   </div>
 </div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Status of Home: </span><span> Clean/Tidy, Age Appropriate Toys Available</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Status of person(s) participating in the visit: </span><span> Attentive to baby</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Status of baby: </span><span> Appropriate nutrition observed, fussy/crying</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs is-1">
+    <span class="textsmallbold">GGK</span>
+  </div>
+  <div class="column p-xxs">
+    <span class="textsmallbold">10-12 Months</span>
+  </div>
+  <div class="column p-xxs is-half">
+    <span></span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Module: </span><span> Basic Care</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Activity: </span><span> Walk</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Subsection: </span><span> Changing Needs of the Home</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Handout: </span><span> Avoiding Accidents</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Demo: </span><span> None</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">List any additional subsections, activities or handouts utilized during the visit: </span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+      <span>Looked over the weaning handout and did crawling activity.</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">List any other materials utilized during the visit:</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+      <span>Blocks</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+      <span class="textsmallbold">Forms completed:</span><span>Contact Information, Child Status, Family/Caregiver Status, HIPPA</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Parent/Child Interaction (describe behaviors and activities observed between the parent and child):</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs centertext">
+    <span class="textsmallbold ">Cues</span>
+  </div>
+  <div class="column p-xxs centertext">
+    <span class="textsmallbold ">Expressions</span>
+  </div>
+  <div class="column p-xxs centertext">
+    <span class="textsmallbold ">Empathy</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs centertext">
+    <span>Baby cried</span>
+  </div>
+  <div class="column p-xxs centertext">
+    <span>Mom said "you're hungry"</span>
+  </div>
+  <div class="column p-xxs centertext">
+    <span>Mom fed baby a bottle.</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Strengths/concerns addressed:</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+    <span>Mom talked about how baby is crying more and thinks maybe it is teething.</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Action tool: </span><span> Explore & Wonder</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column  m-l-sm textsmall p-xxs">
+    <span>FSW and Mom explored any patterns with baby crying, like time of day or how he is feeling.  Mom talked
+    about how the crying often happens before nap time.  We discussed possibly putting baby to sleep a little earlier.  </span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Daily Do's: </span><span> Body Builders</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column  m-l-sm textsmall p-xxs">
+    <span>Mom talked about how she has put gates up around the house so baby can't get in the kitchen and bathrooms.
+    FSW praised Mom for this as it will help baby to stay safe and explore the home without getting hurt. </span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Progress made by family towards goals: </span><span> Progression</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column  m-l-sm textsmall p-xxs">
+    <span>Baby is standing a little and almost walking! </span>
+  </div>
+</div>
 
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Narrative: Describe how person(s) present responded to materials, activities and modules, etc.:</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column  m-l-sm textsmall p-xxs">
+    <span>Sint. Aliquip aute reprehenderit for natus nemo so vitae so error. Eu autem and culpa,
+      minima. Aut nequeporro but totam but beatae. Nisi. Incidunt laudantium or sed nulla. Architecto
+      modi but totam. Non suscipit so corporis. Quam incidunt so dolore yet in but ullam eiusmod or
+      elit. Proident consequuntur numquam quaerat qui for nulla rem. Culpa laborum yet culpa,
+      voluptatem for odit sequi. Nostrum quis but illo. Architecto elit, accusantium veritatis.
 
+      Ut numquam so laudantium exercitationem for aliquam. Unde occaecat yet quo et, velit for
+      duis and ad. Dolorem ratione aspernatur laboriosam qui and consequatur. Quae exercitation,
+      or magnam, nulla. Voluptate eu or lorem so elit and velit, eiusmod commodi. Laboriosam molestiae
+       dolor but in aliqua. Ut ipsum exercitation culpa voluptate aliquam, or velit. Labore sint so et
+       esse ut for aliquid. Beatae omnis, eos sit for perspiciatis. Esse veritatis et dolor but dolor
+       but magna but sunt. Aspernatur eius so ullamco but deserunt. Consectetur. Ullamco omnis illum or
+       suscipit vel for autem elit. Voluptas. Quae ut dicta ut so ad for irure but proident. Tempor id
+       consequatur yet exercitation et quam or exercitationem. Incidunt proident and dolores.
+     </span>
+  </div>
+</div>
+
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Referrals accepted at last visit were acted upon by family:</span>
+    <span>
+  </div>
+  <div class="column p-xxs">
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="foobar" checked>
+          Yes
+        </label>
+        <label class="radio">
+          <input type="radio" name="foobar">
+          No
+        </label>
+        <label class="radio">
+          <input type="radio" name="foobar">
+          None made at last visit
+        </label>
+      </div>
+    </span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Explain: </span><span> Family attended group event.</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column p-xxs">
+    <span class="textsmallbold">Follow up plans:</span>
+    <span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+    <span class="textsmallbold">Module: </span><span> 10-12 Cues and Communication</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+    <span class="textsmallbold">Activity: </span><span> Take a walk</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-l-sm p-xxs">
+    <span class="textsmallbold">Next visit: </span><span> November 7, 2018</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-t-md p-xxs">
+    <span>FSW Signature</span>
+  </div>
+  <div class="column m-t-md p-xxs">
+    <span>Date</span>
+  </div>
+</div>
+<div class="columns">
+  <div class="column m-t-md p-xxs">
+    <span>Supervisor Signature</span>
+  </div>
+  <div class="column m-b-xxl m-t-md p-xxs">
+    <span>Date</span>
+  </div>
+</div>
 @endsection
