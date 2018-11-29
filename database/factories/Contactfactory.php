7e425d1425e1934a3623b851e248f44a665112fe
Holly Stone <hstone102@gmail.com>
2018-11-29 11:02:32 -0500

    contact factory up

---
+++ database/factories/Contactfactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Contact::class, function (Faker $faker) {
+    return [
+
+      'name' => $faker->randomElement(['Mother', 'Father', 'Family Member', 'Grandparent']),
+      'type' => $faker-> randomElement(['No Show', 'Cancel', 'Reschedule', 'Group Event', 'Reminder']),
+      'date' => $faker->dateTimeBetween($startDate = '-4 weeks', $endDate = 'now', $timezone = null),
+      'start' => $faker->time($format = 'H:i:s', $max = 'now'),
+      'end' => $faker->time($format = 'H:i:s', $max = 'now'),
+      'comment' => $faker->text($maxNbChars = 50),
+        //
+    ];
+});
---
+++ database/seeds/ContactsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        'name'
+        'user_id'
+        'type'
+        'date'
+        'start'
+        'end'
+        'comment'
+    }
+}
